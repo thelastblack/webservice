338b5b47de689e01adf5b3de36c58c545a8a3caf
Vahid Fazloollahzade <vahid.fazl2000@engineer.com>
2016-04-08 11:09:43 +0430

    Add unit test for checkInbox method

--- tests/Unit/HttpClientTest.php
+++ tests/Unit/HttpClientTest.php
@@ -1,13 +1,16 @@
 <?php
 namespace OpiloClientTest\Unit;
 
+use DateTime;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Response;
 use Mockery\Mock;
+use OpiloClient\Request\IncomingSMS;
 use OpiloClient\Request\OutgoingSMS;
+use OpiloClient\Response\Inbox;
 use OpiloClient\Response\SMSId;
 use PHPUnit_Framework_TestCase;
 use OpiloClient\V2\HttpClient;
@@ -56,5 +59,57 @@ class HttpClientTest extends PHPUnit_Framework_TestCase {
         // Inspect request
         $this->assertArrayHasKey(0, $container);
         $this->assertArrayHasKey('request', $container[0]);
+    }
+
+    public function testCheckInbox() {
+        // Mock a Guzzle client to be used
+        $responseArray = [
+            'messages' => [
+                12345 => [
+                    'from' => '9121231234',
+                    'to' => '9123214321',
+                    'text' => 'سلام دوست عزیز',
+                    'received_at' => '2016-03-15 14:27:30',
+                ],
+                54321 => [
+                    'to' => '9121231234',
+                    'from' => '9123214321',
+                    'text' => 'Hello mate',
+                    'received_at' => '2012-05-25 02:10:27',
+                ],
+            ]
+        ];
+        $mockedGuzzleClient = new MockHandler([
+            new Response(200, [], json_encode($responseArray))
+        ]);
+        // Also, attach a history middleware to later inspect requests and responses
+        $container = [];
+        $history = Middleware::history($container);
+        $stack = HandlerStack::create($mockedGuzzleClient);
+        $stack->push($history);
+        $client = new Client([
+            'handler' => $stack,
+            'base_url' => getenv('OPILO_URL') . '/ws/api/v2/'
+        ]);
+
+        // Make HttpClient to use mocked Guzzle client
+        $httpClient = new HttpClient('no-need-for-username', 'no-need-for-password', $client);
+
+        // Call checkInbox
+        $result = $httpClient->checkInbox();
+
+        // Inspect $result
+        $this->assertInstanceOf(Inbox::class, $result);
+        $messages = $result->getMessages();
+        $this->assertTrue(is_array($messages));
+        $this->assertCount(2, $messages);
+        $first = $messages[0];
+        $this->assertInstanceOf(IncomingSMS::class, $first);
+        $this->assertEquals(12345, $first->getOpiloId());
+        $this->assertEquals('9121231234', $first->getFrom());
+        $this->assertEquals('9123214321', $first->getTo());
+        $this->assertEquals('سلام دوست عزیز', $first->getText());
+        $this->assertInstanceOf(DateTime::class, $first->getReceivedAt());
+        $this->assertEquals('2016-03-15 14:27:30', $first->getReceivedAt()->format('Y-m-d H:i:s'));
     }
 }
